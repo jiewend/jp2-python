bdcce1fa231e3960fd61cac1a12123026f4f84f1
David Barrat <david.barrat.1@gmail.com>
2018-04-03 12:54:55 +0200

    update

--- src/compression/__init__.py
+++ src/compression/__init__.py
@@ -2,6 +2,7 @@
 'compression methods'
 import pywt
 import numpy
+import src.utils as util
 
 def rgb_to_yuv(img):
     '-'
@@ -77,5 +78,82 @@ def extract_rgb_coeff(img):
     return (coeffs_r, coeffs_g, coeffs_b)
 
 def img_from_dwt_coeff(coeff_dwt):
-    return None
+    #Channel Red
+    (coeffs_r, coeffs_g, coeffs_b) = coeff_dwt
+    cARed = numpy.array(coeffs_r[0])
+    (width, height) = coeffs_r.shape
+    cHRed = numpy.array(coeffs_r[1][0])
+    cVRed = numpy.array(coeffs_r[1][1])
+    cDRed = numpy.array(coeffs_r[1][2])
+    #Channel Green
+    cAGreen = numpy.array(coeffs_g[0])
+    cHGreen = numpy.array(coeffs_g[1][0])
+    cVGreen = numpy.array(coeffs_g[1][1])
+    cDGreen = numpy.array(coeffs_g[1][2])
+    #Channel Blue
+    cABlue = numpy.array(coeffs_b[0])
+    cHBlue = numpy.array(coeffs_b[1][0])
+    cVBlue = numpy.array(coeffs_b[1][1])
+    cDBlue = numpy.array(coeffs_b[1][2])
 
+    # We're computing there the maxValue per channel par matrix
+    cAMaxRed = util.max_ndarray(cARed)
+    print (cAMaxRed)
+    # cAMaxRed   = maxVal(cARed)
+    # cAMaxGreen = maxVal(cAGreen)
+    # cAMaxBlue  = maxVal(cABlue)
+    #
+    # cHMaxRed   = maxVal(cHRed)
+    # cHMaxGreen = maxVal(cHGreen)
+    # cHMaxBlue  = maxVal(cHBlue)
+    #
+    # cVMaxRed   = maxVal(cVRed)
+    # cVMaxGreen = maxVal(cVGreen)
+    # cVMaxBlue  = maxVal(cVBlue)
+    #
+    # cDMaxRed   = maxVal(cDRed)
+    # cDMaxGreen = maxVal(cDGreen)
+    # cDMaxBlue  = maxVal(cDBlue)
+    #
+    # dwt_img = Image.new("RGB",(width*2,height*2),(0,0,20))
+    # #cA reconstruction
+    # for i in range(width):
+    #     for j in range(height):
+    #         R = cARed[i][j]
+    #         R = (R/cAMaxRed)*160.0
+    #         G = cAGreen[i][j]
+    #         G = (G/cAMaxGreen)*85.0
+    #         B = cABlue[i][j]
+    #         B = (B/cAMaxBlue)*100.0
+    #         dwt_img.putpixel((i,j) , (int(R),int(G),int(B)) )
+    # #cH reconstruction
+    # for i in range(width):
+    #     for j in range(height):
+    #         R = cHRed[i][j]
+    #         R = (R/cHMaxRed)*160.0
+    #         G = cHGreen[i][j]
+    #         G = (G/cHMaxGreen)*85.0
+    #         B = cHBlue[i][j]
+    #         B = (B/cHMaxBlue)*100.0
+    #         dwt_img.putpixel((i+width,j) , (int(R),int(G),int(B)) )
+    # #cV reconstruction
+    # for i in range(width):
+    #     for j in range(height):
+    #         R = cVRed[i][j]
+    #         R = (R/cVMaxRed)*160.0
+    #         G = cVGreen[i][j]
+    #         G = (G/cVMaxGreen)*85.0
+    #         B = cVBlue[i][j]
+    #         B = (B/cVMaxBlue)*100.0
+    #         dwt_img.putpixel((i,j+height) , (int(R),int(G),int(B)) )
+    # #cD reconstruction
+    # for i in range(width):
+    #     for j in range(height):
+    #         R = cDRed[i][j]
+    #         R = (R/cDMaxRed)*160.0
+    #         G = cDGreen[i][j]
+    #         G = (G/cDMaxGreen)*85.0
+    #         B = cDBlue[i][j]
+    #         B = (B/cDMaxBlue)*100.0
+    #         dwt_img.putpixel((i+width,j+height) , (int(R),int(G),int(B)) )
+    return dwt_img
